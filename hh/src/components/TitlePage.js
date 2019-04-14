437ab6df6ed99f6f710fb155ec05fbf4ea0602bd
Sophia Yao <sy2743@columbia.edu>
2019-04-14 16:01:04 -0400

    title page

--- hh/src/components/TitlePage.js
+++ hh/src/components/TitlePage.js
@@ -5,15 +5,23 @@ import styled from 'styled-components';
 const Title = styled.div`
   font-size : 4.0rem;
   color:white;
-  position: absolute;
   font-family: 'Open Sans', sans-serif;
-  bottom: 0;
   font-weight:700;
+  position: absolute;
+  bottom: 0;
+  margin-right: auto;
+  margin-left: auto;
+  margin-bottom: -1.2rem;
+  left:0;
+  right:0;
 `
 
 const Titles = styled.div`
   position: relative;
   height:30rem;
+  text-align:center;
+  background-position: center; 
+  background-repeat: no-repeat;
   background-image: url("https://cdn.pixabay.com/photo/2016/02/09/12/25/puppy-1189067_960_720.jpg");
 `
 
