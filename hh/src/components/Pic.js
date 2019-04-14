3b436a08efa2bc42af67d9469798bf2c9e663314
Kenevian <Katon.vanetten@gmail.com>
2019-04-14 15:21:10 -0400

    Skeleton Pic Comp

---
+++ hh/src/components/Pic.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+let Image = styled.div`
+
+`
+
+class Image extends Component {
+    render(){
+        return (
+            <Image></Image>
+        );
+    }
+}
+
+export default Image;
