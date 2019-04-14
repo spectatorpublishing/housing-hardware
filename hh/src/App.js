b4186d9b11f7fef2583f450ad2bd50b098c76656
Rounak Bera <rounakbera@gmail.com>
2019-04-14 15:27:24 -0400

    fonts

--- hh/src/App.js
+++ hh/src/App.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 import QandA from './components/QandA.js';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css?family=Open+Sans:400,700|Raleway');
+
+body{
+	margin: 0;
+    padding: 0;
+    font-family: "Raleway", -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+      "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+}
 
 class App extends Component {
   render() {
     return (
-      <div>>
+      <div>
         <QandA question = "Hello?" answer = "Hello!"></QandA>
       </div>
     );
